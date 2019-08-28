115869190398387a73b34a1e5f2b833a93562e36
Евгений Красильников <Kras076.99@gmail.com>
2019-08-28 17:49:40 +0300

    Add Guest model

---
+++ Restaurant/Migrations/201908281302386_AddGuestsTable.cs
@@ -0,0 +1,30 @@
+namespace Restaurant.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddGuestsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Guests",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Guests", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.Guests", new[] { "UserId" });
+            DropTable("dbo.Guests");
+        }
+    }
+}
