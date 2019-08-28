a613314327912e99d6152606ef64f16119da8ebd
Евгений Красильников <Kras076.99@gmail.com>
2019-08-29 02:05:11 +0300

    Check User Roles in HomeController

--- Restaurant/Controllers/HomeController.cs
+++ Restaurant/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace Restaurant.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            string id = User.Identity.GetUserId();
+            if (User.IsInRole("waiter"))
+                return RedirectToAction("Index", "Waiter", new { id });
+            if (User.IsInRole("manager"))
+                return RedirectToAction("Index", "Manager", new { id });
             return View();
         }
 
