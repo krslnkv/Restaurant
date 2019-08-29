fdb78a39e79136a0af215c47302612bf88543f1f
Евгений Красильников <Kras076.99@gmail.com>
2019-08-29 20:54:51 +0300

    Add NewOrderModel

---
+++ Restaurant/Models/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant.Models
+{
+    public class NewOrderModel
+    {
+        public string GuestEmail { get; set; }
+        public string GuestName { get; set; }
+        public int TableId { get; set; }
+        public int WaiterId { get; set; }
+
+    }
+}
